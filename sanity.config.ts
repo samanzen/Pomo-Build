d019b1553befeb2ad0d3bf7f3a24203f8781d361
samanzen <kh.sa62@gmail.com>
2025-08-12 16:48:47 -0700

    Successfully configured and installed Sanity CMS

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,28 +1,22 @@
-'use client'
-
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
- */
-
-import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schema} from './sanity/schemas' // Corrected import name
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+// Get the project ID and dataset from your .env.local file
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
 export default defineConfig({
   basePath: '/studio',
+  name: 'Pomo_Build_Content_Studio',
+  title: 'Pomo Build CMS',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    deskTool(),
+    visionTool(),
   ],
+  // Use the imported schema directly
+  schema: schema,
 })
