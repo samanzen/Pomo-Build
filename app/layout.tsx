c74e55eb1774a53cfd19506db020843fa3a175dd
samanzen <kh.sa62@gmail.com>
2025-08-26 12:36:23 -0700

    Manually linked favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" />
         <SchemaMarkup />
         {/* Google Analytics */}
         <Script
